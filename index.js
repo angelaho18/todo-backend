7f2c90acff653d5f096327f0b91733568287044a
angelaho18 <angelllaaaaaaaa@gmail.com>
2021-05-24 20:54:39 +0700

    backend update

--- index.js
+++ index.js
@@ -21,17 +21,17 @@ connection.connect(function(err){
     console.log("yeee connected !!!")
 })
 
-app.get('/',(req,res)=>{
-    res.send(
-        `<html>
-            <form action="/todo" method="POST">
-                <label>todo</label>
-                <input name = "todo"></input>
-                <button>Submit</button>
-            </form>
-        </html>`
-    )
-})
+// app.get('/',(req,res)=>{
+//     res.send(
+//         `<html>
+//             <form action="/todo" method="POST">
+//                 <label>todo</label>
+//                 <input name = "catatan"></input>
+//                 <button>Submit</button>
+//             </form>
+//         </html>`
+//     )
+// })
 
 app.get('/todo',(req,res)=>{
     connection.connect(function(err) {
@@ -45,9 +45,8 @@ app.get('/todo',(req,res)=>{
     });
 })
 app.post('/todo',(req,res)=>{
-    var isi = req.body.todo
-    //req.body.catatan
-    console.log(req.body.todo)
+    var isi = req.body.catatan
+    console.log(req.body.catatan)
     var content = "INSERT INTO daftartodo (catatan) VALUES (?)"
     connection.query(content,isi, function (err, result) {
         if (err) throw err;
@@ -58,8 +57,90 @@ app.post('/todo',(req,res)=>{
     res.end()
 })
 
-//hps?
+app.delete('/todo/:id',(req,res)=>{
+    var content = "DELETE from daftartodo WHERE id = (?)"
+    connection.query(content,req.params.id, function (err, result) {
+        console.log(req.params.id)
+        if (err) throw err;
+        console.log("1 record deleted");
+    });
+    res.end()
+})
 
-// app.delete('/todo/'(req,res))
+
+// app.get('/',(req,res)=>{
+//     res.send(
+//         `<html>
+//             <form action="/user" method="POST">
+//                 <label>username</label>
+//                 <input name = "username1"></input>
+//                 <label>password</label>
+//                 <input name = "password1"></input>
+//                 <button> Login</button>
+//             </form>
+//         </html>`
+//     )
+// })
+
+app.post('/userAuth',(req,res)=>{
+    try{
+        const {username,password} = req.body
+        // console.log(username)
+        // console.log(password)
+        if(!username || !password){
+            return res.status(400).send("fail")
+        }
+        connection.query('SELECT * FROM user WHERE username = (?) AND password = (?)', [username,password], function(err,result){ 
+            if(result.length <= 0){
+                console.log(result.length)
+                console.log(result)
+                res.status(401).send("fail")
+                console.log("login fail")
+            }else{
+                console.log(result.length)
+                console.log(result)
+                // res.redirect('/todo')
+                console.log("success login")
+            }
+        })
+    }catch(err){
+        console.log(err)
+    }
+})
+
+
+app.post('/user',(req,res)=>{
+    var isi = [req.body.username, req.body.password]
+    console.log(req.body.catatan)
+    var content = "INSERT INTO user (username,password) VALUES (?,?)"
+    connection.query(content,isi, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+    });
+    console.log('sudah terhandle oleh post di /user')
+    console.log(req.body)
+    res.end()
+})
+
+app.get('/user',(req,res)=>{
+    connection.connect(function(err) {
+        connection.query("SELECT * FROM user", function (err, result, fields) {
+            if (err) throw err;
+            res.json(result)
+        });
+    });
+})
+
+app.delete('/user/:id',(req,res)=>{
+    var content = "DELETE from user WHERE userid = (?)"
+    connection.query(content,req.params.id, function (err, result) {
+        console.log(req.params.id)
+        if (err) 
+            throw err;
+        else
+            console.log("1 record deleted");
+    });
+    res.end()
+})
 
 app.listen(3000, ()=> console.log(`server started on 3000`))
--- query1.sql
+++ query1.sql
@@ -2,8 +2,15 @@
 
 -- show DATABASE
 
-CREATE TABLE daftartodo(
-    id int NOT NULL AUTO_INCREMENT,
-    catatan VARCHAR(100),
-    PRIMARY KEY(id)
+-- CREATE TABLE daftartodo(
+--     id int NOT NULL AUTO_INCREMENT,
+--     catatan VARCHAR(100),
+--     PRIMARY KEY(id)
+-- );
+
+CREATE TABLE user(
+    userid int NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50),
+    password VARCHAR(50),
+    PRIMARY KEY(userid)
 );
